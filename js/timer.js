a2f845bf66ed164eaf92bcdf3b3b5e4aefec6f91
AKHJDSGBBJC <ryanzhao48@gmail.com>
2025-07-21 11:19:13 -0400

    s

---
+++ js/timer.js
@@ -0,0 +1,37 @@
+const startBtn = document.getElementById('startBtn');
+const minutesInput = document.getElementById('minutesInput');
+const countdownDisplay = document.getElementById('countdownDisplay');
+let timerInterval;
+
+function formatTime(seconds) {
+  const m = Math.floor(seconds / 60);
+  const s = seconds % 60;
+  return `${m.toString().padStart(2, '0')}:${s.toString().padStart(2, '0')}`;
+}
+
+startBtn.addEventListener('click', () => {
+    clearInterval(timerInterval);
+  
+    let totalSeconds = parseInt(minutesInput.value) * 60;
+  
+    if (isNaN(totalSeconds) || totalSeconds <= 0) {
+      minutesInput.value = "Please enter a valid number!";
+      return;
+    }
+  
+    minutesInput.readOnly = true;
+    minutesInput.value = formatTime(totalSeconds);
+  
+    timerInterval = setInterval(() => {
+      totalSeconds--;
+  
+      if (totalSeconds < 0) {
+        clearInterval(timerInterval);
+        minutesInput.value = '';
+        minutesInput.readOnly = false;
+        return;
+      }
+  
+      minutesInput.value = formatTime(totalSeconds);
+    }, 1000);
+  });
